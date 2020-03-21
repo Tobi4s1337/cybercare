4f51f14728ff858b1c95042c13ceba43a22965fe
Tobias Klaus <tk@nuiteq.com>
2020-03-22 00:17:46 +0100

    fix twilio

--- routes/twilioRoutes.js
+++ routes/twilioRoutes.js
@@ -21,10 +21,10 @@ router.post('/priority', (request, response) => {
 		console.log(request.body.Digits);
 		if (request.body.Digits.length === 5) {
 			console.log('redirecting to record');
-			twiml.redirect('../record');
+			twiml.redirect('http://212.8.251.192:3001/voice/record');
 		} else {
 			console.log('redirecting to postal');
-			twiml.redirect('../postal');
+			twiml.redirect('http://212.8.251.192:3001/voice/postal');
 		}
 	}
 
