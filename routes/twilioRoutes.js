5b9e9813215494b99c4d26a8f0713c0c54e58754
Tobias Klaus <tk@nuiteq.com>
2020-03-22 00:58:17 +0100

    fix twilio

--- routes/twilioRoutes.js
+++ routes/twilioRoutes.js
@@ -6,25 +6,14 @@ router.post('/record', (request, response) => {
 	// Use the Twilio Node.js SDK to build an XML response
 	const twiml = new VoiceResponse();
 
-	// Use the <Gather> verb to collect user input
-	const gatherUrgency = twiml.gather({
-		numDigits: 1
-	});
-	gatherUrgency.say(
-		{
-			voice: 'woman',
-			language: 'de-DE'
-		},
-		'Geben tippen sie bitte ihre postleitzahl ein.'
-	);
-
 	if (request.body.Digits) {
+		console.log(request.body.Digits);
 		twiml.say(
 			{
 				voice: 'woman',
 				language: 'de-DE'
 			},
-			'Bitte melden Sie Ihr Problem und sagen sie wie dringend sie hilfe benoetigen'
+			'Bitte melden Sie Ihr Problem und sagen sie wie dringend sie hilfe benötigen'
 		);
 
 		// Use <Record> to record and transcribe the caller's message
@@ -36,8 +25,17 @@ router.post('/record', (request, response) => {
 		// End the call with <Hangup>
 		twiml.hangup();
 	} else {
-		// If the user doesn't enter input, loop
-		twiml.redirect('../record');
+		// Use the <Gather> verb to collect user input
+		const gatherUrgency = twiml.gather({
+			numDigits: 5
+		});
+		gatherUrgency.say(
+			{
+				voice: 'woman',
+				language: 'de-DE'
+			},
+			'Geben tippen sie bitte ihre postleitzahl ein.'
+		);
 	}
 
 	// Render the response as XML in reply to the webhook request
