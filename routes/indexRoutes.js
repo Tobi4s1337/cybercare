6ed2390de5570293a2b031ebb757e12cd1e5339e
Tobias Klaus <tk@nuiteq.com>
2020-03-21 04:08:29 +0100

    add background

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-	res.render('landing');
+	res.render('landing', { page: 'landing' });
 });
 
 router.post('/faq', function(req, res) {
