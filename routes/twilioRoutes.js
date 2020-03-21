a6d89078ae186db34ffbe991f53cf6ba9c8a2b49
Tobias Klaus <tk@nuiteq.com>
2020-03-21 22:17:15 +0100

    add music

--- routes/twilioRoutes.js
+++ routes/twilioRoutes.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
-
+const path = require('path');
 const router = express.Router();
 
 // Returns TwiML which prompts the caller to record a message
+router.get('/music', (req, res) => {
+	res.sendFile(path.resolve('public/corona.mp3'));
+});
+
 router.post('/record', (request, response) => {
 	// Use the Twilio Node.js SDK to build an XML response
 	const twiml = new VoiceResponse();
-	twiml.say('Hello. Please leave a message after the beep. Unless!');
+
+	twiml.play('http://212.8.251.192:3000/voice/music');
+
+	twiml.say(
+		{
+			voice: 'woman',
+			language: 'de-DE'
+		},
+		'Bitte melden Sie Ihr Problem. Unless!'
+	);
 
 	// Use <Record> to record and transcribe the caller's message
 	twiml.record({ transcribe: true, maxLength: 30 });
