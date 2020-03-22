de2f89d1afbd8a15d853281e36698074d38c882b
Dominic <d.kloecker@icloud.com>
2020-03-22 04:11:35 +0000

    Commit for polish

--- routes/twilioRoutes.js
+++ routes/twilioRoutes.js
@@ -1,16 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const router = express.Router();
 const Victim = require('../models/victim');
 const Place = require('../models/place');
+const accountSid = 'AC1b3ea53ce52ee15a50d7672dc6b85416';
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 
 router.post('/record', (req, res) => {
 	// Use the Twilio Node.js SDK to build an XML response
 	const twiml = new VoiceResponse();
 	if (req.body.Digits) {
 		console.log(req.body.Digits);
-		twiml.say(
-			{
+		twiml.say({
 				voice: 'Polly.Vicki',
 				language: 'de-DE'
 			},
@@ -30,8 +34,7 @@ router.post('/record', (req, res) => {
 		const gatherUrgency = twiml.gather({
 			numDigits: 5
 		});
-		gatherUrgency.say(
-			{
+		gatherUrgency.say({
 				voice: 'Polly.Vicki',
 				language: 'de-DE'
 			},
@@ -42,8 +45,7 @@ router.post('/record', (req, res) => {
 	console.log(req.body);
 
 	if (req.body.Digits && req.body.Digits.length === 5) {
-		Victim.create(
-			{
+		Victim.create({
 				_id: req.body.CallSid,
 				postalCode: req.body.Digits,
 				phoneNumber: req.body.phoneNumber
@@ -54,48 +56,52 @@ router.post('/record', (req, res) => {
 			}
 		);
 	} else if (req.body.RecordingUrl) {
-		Victim.findByIdAndUpdate(
-			req.body.CallSid,
-			{
-				problem: {
-					audioFile: req.body.RecordingUrl
-				}
-			},
-			(err, victim) => {
-				if (err) console.log(err);
-				console.log('victim updated');
-				console.log(victim);
-				Place.findById(victim.postalCode, (err, place) => {
-					if (err) console.log(err);
-					if (place) {
-						let victims = place.victims;
-						victims.push(victim._id);
-						Place.findByIdAndUpdate(
-							victim.postalCode,
-							{
-								victims: victims
-							},
-							(err, place) => {
-								console.log('added victim to place');
-								console.log(place);
+
+		client.transcriptions('TRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+			.fetch()
+			.then(transcription => {
+				Victim.findByIdAndUpdate(
+					req.body.CallSid, {
+						problem: {
+							audioFile: req.body.RecordingUrl
+						}
+					},
+					(err, victim) => {
+						if (err) console.log(err);
+						console.log('victim updated');
+						console.log(victim);
+						Place.findById(victim.postalCode, (err, place) => {
+							if (err) console.log(err);
+							if (place) {
+								let victims = place.victims;
+								victims.push(victim._id);
+								Place.findByIdAndUpdate(
+									victim.postalCode, {
+										victims: victims
+									},
+									(err, place) => {
+										console.log('added victim to place');
+										console.log(place);
+									}
+								);
+							} else {
+								Place.create({
+										_id: victim.postalCode,
+										victims: [victim._id]
+									},
+									(err, place) => {
+										if (err) console.log(err);
+										console.log('Created place');
+										console.log(place);
+									}
+								);
 							}
-						);
-					} else {
-						Place.create(
-							{
-								_id: victim.postalCode,
-								victims: [ victim._id ]
-							},
-							(err, place) => {
-								if (err) console.log(err);
-								console.log('Created place');
-								console.log(place);
-							}
-						);
+						});
 					}
-				});
-			}
-		);
+				);
+			});
+
+
 	}
 
 	// Render the response as XML in reply to the webhook request
