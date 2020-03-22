97bc28f9a64acff8d6f0f4b414388efc568eed12
Tobias Klaus <tk@nuiteq.com>
2020-03-22 06:13:41 +0100

    remove transcribe feature

--- routes/twilioRoutes.js
+++ routes/twilioRoutes.js
@@ -1,55 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const router = express.Router();
 const Victim = require('../models/victim');
 const Place = require('../models/place');
-
-router.post('/transcribe', (req, res) => {
-	console.log(req.body);
-	Victim.findByIdAndUpdate(
-		req.body.CallSid,
-		{
-			problem: {
-				audioFile: req.body.RecordingUrl,
-				text: req.body.TranscriptionText
-			}
-		},
-		(err, victim) => {
-			if (err) console.log(err);
-			console.log('victim updated');
-			console.log(victim);
-			Place.findById(victim.postalCode, (err, place) => {
-				if (err) console.log(err);
-				if (place) {
-					let victims = place.victims;
-					victims.push(victim._id);
-					Place.findByIdAndUpdate(
-						victim.postalCode,
-						{
-							victims: victims
-						},
-						(err, place) => {
-							console.log('added victim to place');
-							console.log(place);
-						}
-					);
-				} else {
-					Place.create(
-						{
-							_id: victim.postalCode,
-							victims: [ victim._id ]
-						},
-						(err, place) => {
-							if (err) console.log(err);
-							console.log('Created place');
-							console.log(place);
-						}
-					);
-				}
-			});
-		}
-	);
-});
+const accountSid = 'AC1b3ea53ce52ee15a50d7672dc6b85416';
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 router.post('/record', (req, res) => {
 	// Use the Twilio Node.js SDK to build an XML response
@@ -66,9 +23,8 @@ router.post('/record', (req, res) => {
 
 		// Use <Record> to record and transcribe the caller's message
 		twiml.record({
-			transcribeCallback: '/voice/transcribe',
-			maxLength: 30,
-			language: 'de-DE'
+			transcribeCallback: true,
+			maxLength: 30
 		});
 
 		// End the call with <Hangup>
@@ -94,13 +50,58 @@ router.post('/record', (req, res) => {
 			{
 				_id: req.body.CallSid,
 				postalCode: req.body.Digits,
-				phoneNumber: req.body.Caller
+				phoneNumber: req.body.phoneNumber
 			},
 			(err) => {
 				if (err) console.log(err);
 				console.log('created new victim');
 			}
 		);
+	} else if (req.body.RecordingUrl) {
+		client.transcriptions('TRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX').fetch().then((transcription) => {
+			Victim.findByIdAndUpdate(
+				req.body.CallSid,
+				{
+					problem: {
+						audioFile: req.body.RecordingUrl
+					}
+				},
+				(err, victim) => {
+					if (err) console.log(err);
+					console.log('victim updated');
+					console.log(victim);
+					Place.findById(victim.postalCode, (err, place) => {
+						if (err) console.log(err);
+						if (place) {
+							let victims = place.victims;
+							victims.push(victim._id);
+							Place.findByIdAndUpdate(
+								victim.postalCode,
+								{
+									victims: victims
+								},
+								(err, place) => {
+									console.log('added victim to place');
+									console.log(place);
+								}
+							);
+						} else {
+							Place.create(
+								{
+									_id: victim.postalCode,
+									victims: [ victim._id ]
+								},
+								(err, place) => {
+									if (err) console.log(err);
+									console.log('Created place');
+									console.log(place);
+								}
+							);
+						}
+					});
+				}
+			);
+		});
 	}
 
 	// Render the response as XML in reply to the webhook request
