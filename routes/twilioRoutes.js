7e54ba9a42c47838da5dbe329067af751ce96f9d
Tobias Klaus <tk@nuiteq.com>
2020-03-22 05:55:23 +0100

    add transcript feature

--- routes/twilioRoutes.js
+++ routes/twilioRoutes.js
@@ -8,13 +8,60 @@ const accountSid = 'AC1b3ea53ce52ee15a50d7672dc6b85416';
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+router.post('/transcribe', (req, res) => {
+	console.log(req.body);
+	Victim.findByIdAndUpdate(
+		req.body.CallSid,
+		{
+			problem: {
+				audioFile: req.body.RecordingUrl,
+				text: req.body.TranscriptionText
+			}
+		},
+		(err, victim) => {
+			if (err) console.log(err);
+			console.log('victim updated');
+			console.log(victim);
+			Place.findById(victim.postalCode, (err, place) => {
+				if (err) console.log(err);
+				if (place) {
+					let victims = place.victims;
+					victims.push(victim._id);
+					Place.findByIdAndUpdate(
+						victim.postalCode,
+						{
+							victims: victims
+						},
+						(err, place) => {
+							console.log('added victim to place');
+							console.log(place);
+						}
+					);
+				} else {
+					Place.create(
+						{
+							_id: victim.postalCode,
+							victims: [ victim._id ]
+						},
+						(err, place) => {
+							if (err) console.log(err);
+							console.log('Created place');
+							console.log(place);
+						}
+					);
+				}
+			});
+		}
+	);
+});
 
 router.post('/record', (req, res) => {
 	// Use the Twilio Node.js SDK to build an XML response
 	const twiml = new VoiceResponse();
 	if (req.body.Digits) {
 		console.log(req.body.Digits);
-		twiml.say({
+		twiml.say(
+			{
 				voice: 'Polly.Vicki',
 				language: 'de-DE'
 			},
@@ -23,7 +70,7 @@ router.post('/record', (req, res) => {
 
 		// Use <Record> to record and transcribe the caller's message
 		twiml.record({
-			transcribe: true,
+			transcribeCallback: '/voice/transcribe',
 			maxLength: 30
 		});
 
@@ -34,7 +81,8 @@ router.post('/record', (req, res) => {
 		const gatherUrgency = twiml.gather({
 			numDigits: 5
 		});
-		gatherUrgency.say({
+		gatherUrgency.say(
+			{
 				voice: 'Polly.Vicki',
 				language: 'de-DE'
 			},
@@ -45,7 +93,8 @@ router.post('/record', (req, res) => {
 	console.log(req.body);
 
 	if (req.body.Digits && req.body.Digits.length === 5) {
-		Victim.create({
+		Victim.create(
+			{
 				_id: req.body.CallSid,
 				postalCode: req.body.Digits,
 				phoneNumber: req.body.phoneNumber
@@ -56,52 +105,7 @@ router.post('/record', (req, res) => {
 			}
 		);
 	} else if (req.body.RecordingUrl) {
-
-		client.transcriptions('TRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-			.fetch()
-			.then(transcription => {
-				Victim.findByIdAndUpdate(
-					req.body.CallSid, {
-						problem: {
-							audioFile: req.body.RecordingUrl
-						}
-					},
-					(err, victim) => {
-						if (err) console.log(err);
-						console.log('victim updated');
-						console.log(victim);
-						Place.findById(victim.postalCode, (err, place) => {
-							if (err) console.log(err);
-							if (place) {
-								let victims = place.victims;
-								victims.push(victim._id);
-								Place.findByIdAndUpdate(
-									victim.postalCode, {
-										victims: victims
-									},
-									(err, place) => {
-										console.log('added victim to place');
-										console.log(place);
-									}
-								);
-							} else {
-								Place.create({
-										_id: victim.postalCode,
-										victims: [victim._id]
-									},
-									(err, place) => {
-										if (err) console.log(err);
-										console.log('Created place');
-										console.log(place);
-									}
-								);
-							}
-						});
-					}
-				);
-			});
-
-
+		client.transcriptions('TRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX').fetch().then((transcription) => {});
 	}
 
 	// Render the response as XML in reply to the webhook request
