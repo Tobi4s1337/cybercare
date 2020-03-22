79325fcce495c80913fa5c2b98707b3bde3b920a
Tobias Klaus <tk@nuiteq.com>
2020-03-22 13:57:27 +0100

    fix places template

--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -15,7 +15,8 @@ router.get('/', (req, res) => {
 			});
 		} else {
 			res.render('places', {
-				page: 'places'
+				page: 'places',
+				place: false
 			});
 		}
 	});
@@ -27,34 +28,44 @@ router.get('/', (req, res) => {
 	});
 });
 
-router.post('/', function (req, res) {
-	Volunteer.create({
-		email: req.body.email,
-		postalCode: req.params.id
-	}, (err, volunteer) => {
-		if (err) console.log(err);
-		Place.findById(req.param.id, (err, place) => {
+router.post('/', function(req, res) {
+	Volunteer.create(
+		{
+			email: req.body.email,
+			postalCode: req.params.id
+		},
+		(err, volunteer) => {
 			if (err) console.log(err);
-			if (place) {
-				let volunteers = place.volunteers;
-				volunteer.push(volunteer._id);
-				Place.findByIdAndUpdate(req.params.id, {
-					volunteers: volunteers
-				}, (err, place) => {
-					if (err) console.log(err);
-					console.log('Place updated');
-				});
-			} else {
-				Place.create({
-					_id: req.params.id,
-					volunteers: [volunteer._id]
-				}, (err, place) => {
-					if (err) console.log(err);
-					console.log('New place created');
-				});
-			}
-		});
-	});
+			Place.findById(req.param.id, (err, place) => {
+				if (err) console.log(err);
+				if (place) {
+					let volunteers = place.volunteers;
+					volunteer.push(volunteer._id);
+					Place.findByIdAndUpdate(
+						req.params.id,
+						{
+							volunteers: volunteers
+						},
+						(err, place) => {
+							if (err) console.log(err);
+							console.log('Place updated');
+						}
+					);
+				} else {
+					Place.create(
+						{
+							_id: req.params.id,
+							volunteers: [ volunteer._id ]
+						},
+						(err, place) => {
+							if (err) console.log(err);
+							console.log('New place created');
+						}
+					);
+				}
+			});
+		}
+	);
 });
 
 module.exports = router;
