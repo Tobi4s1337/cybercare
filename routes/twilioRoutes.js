b51d2cf9ffc681386f4cacb771f0787842beadda
Tobias Klaus <tk@nuiteq.com>
2020-03-22 04:25:10 +0100

    fix database operations

--- routes/twilioRoutes.js
+++ routes/twilioRoutes.js
@@ -9,7 +9,8 @@ router.post('/record', (req, res) => {
 	const twiml = new VoiceResponse();
 	if (req.body.Digits) {
 		console.log(req.body.Digits);
-		twiml.say({
+		twiml.say(
+			{
 				voice: 'Polly.Vicki',
 				language: 'de-DE'
 			},
@@ -29,7 +30,8 @@ router.post('/record', (req, res) => {
 		const gatherUrgency = twiml.gather({
 			numDigits: 5
 		});
-		gatherUrgency.say({
+		gatherUrgency.say(
+			{
 				voice: 'Polly.Vicki',
 				language: 'de-DE'
 			},
@@ -40,7 +42,8 @@ router.post('/record', (req, res) => {
 	console.log(req.body);
 
 	if (req.body.Digits && req.body.Digits.length === 5) {
-		Victim.create({
+		Victim.create(
+			{
 				_id: req.body.CallSid,
 				postalCode: req.body.Digits,
 				phoneNumber: req.body.phoneNumber
@@ -51,35 +54,48 @@ router.post('/record', (req, res) => {
 			}
 		);
 	} else if (req.body.RecordingUrl) {
-		Victim.findByIdAndUpdate(req.body.CallSid, {
-			problem: {
-				audioFile: req.body.RecordingUrl
-			}
-		}, (err, victim) => {
-			if (err) console.log(err);
-			console.log('victim updated');
-			console.log(victim);
-			Place.findById(victim.postalCode, (err, place) => {
-				if (err) console.log(err);
-				if (place) {
-					let victims = place.victims;
-					victims.push(victim._id);
-					Place.findByIdAndUpdate(victim.postalCode, {
-						victims: victims
-					}, (err, place) => {
-						console.log('added victim to place');
-					});
-				} else {
-					Place.create({
-						_id: victim.postalCode,
-						victims: [victim._id]
-					}, (err, place) => {
-						if (err) console.log(err);
-						console.log('Created place');
-					});
+		Victim.findByIdAndUpdate(
+			req.body.CallSid,
+			{
+				problem: {
+					audioFile: req.body.RecordingUrl
 				}
-			});
-		});
+			},
+			(err, victim) => {
+				if (err) console.log(err);
+				console.log('victim updated');
+				console.log(victim);
+				Place.findById(victim.postalCode, (err, place) => {
+					if (err) console.log(err);
+					if (place) {
+						let victims = place.victims;
+						victims.push(victim._id);
+						Place.findByIdAndUpdate(
+							victim.postalCode,
+							{
+								victims: victims
+							},
+							(err, place) => {
+								console.log('added victim to place');
+								console.log(place);
+							}
+						);
+					} else {
+						Place.create(
+							{
+								_id: victim.postalCode,
+								victims: [ victim._id ]
+							},
+							(err, place) => {
+								if (err) console.log(err);
+								console.log('Created place');
+								console.log(place);
+							}
+						);
+					}
+				});
+			}
+		);
 	}
 
 	// Render the response as XML in reply to the webhook request
