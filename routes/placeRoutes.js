07973c221b06449a8bc94e869e5086a2d99bdb55
Dominic <d.kloecker@icloud.com>
2020-03-22 12:11:18 +0000

    Contact page

--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const router = express.Router({ mergeParams: true });
+const router = express.Router({
+	mergeParams: true
+});
 const Volunteer = require('../models/volunteer');
 const Place = require('../models/place');
 
@@ -9,20 +11,34 @@ router.get('/', (req, res) => {
 	});
 });
 
-router.post('/', function(req, res) {
-	Volunteer.create({ email: req.body.email, postalCode: req.params.id }, (err, volunteer) => {
+router.get('/', (req, res) => {
+	res.render('kontakt', {
+		page: 'kontakt'
+	});
+});
+
+router.post('/', function (req, res) {
+	Volunteer.create({
+		email: req.body.email,
+		postalCode: req.params.id
+	}, (err, volunteer) => {
 		if (err) console.log(err);
 		Place.findById(req.param.id, (err, place) => {
 			if (err) console.log(err);
 			if (place) {
 				let volunteers = place.volunteers;
 				volunteer.push(volunteer._id);
-				Place.findByIdAndUpdate(req.params.id, { volunteers: volunteers }, (err, place) => {
+				Place.findByIdAndUpdate(req.params.id, {
+					volunteers: volunteers
+				}, (err, place) => {
 					if (err) console.log(err);
 					console.log('Place updated');
 				});
 			} else {
-				Place.create({ _id: req.params.id, volunteers: [ volunteer._id ] }, (err, place) => {
+				Place.create({
+					_id: req.params.id,
+					volunteers: [volunteer._id]
+				}, (err, place) => {
 					if (err) console.log(err);
 					console.log('New place created');
 				});
